b0611e6ea24d616d49655537b324cd5d89b0467f
DXM17 <63446875+DXM17@users.noreply.github.com>
2020-04-10 13:25:57 +0100

    Amended the code to create a new line before adding in a new quote

--- get-quote.py
+++ get-quote.py
@@ -1,3 +1,13 @@
+# Open quotes.txt with access mode 'a'
+file_object = open('quotes.txt', 'a')
+
+# Append 'This quote is better than the last quote' at the end of the file
+file_object.write('\n')
+file_object.write('This quote is better than the last quote')
+
+# Close quotes.txt
+file_object.close()
+
 import random
 def primary():
   #print("Keep it logically awesome dude.")
